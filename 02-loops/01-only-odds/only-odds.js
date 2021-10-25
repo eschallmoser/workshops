3a6fc65bb3e1d9785be906395916eeed148d256d
Erin Schallmoser <dialogofadream@gmail.com>
2021-10-24 21:11:57 -0700

    solutions to bootcamp prep workshops

--- 02-loops/01-only-odds/only-odds.js
+++ 02-loops/01-only-odds/only-odds.js
@@ -1,1 +1,15 @@
+// Define a function, `onlyOdds`, that accepts a number as an argument. `onlyOdds`
+// should should return the sum of all the odd numbers between the given number
+// and 1.
+
+// If `onlyOdds` receives an argument less than 1, it should return 0.
+
 // YOUR CODE BELOW
+
+const onlyOdds = (num) => {
+  let sum = 0;
+  for (let i = num; i >= 1; i--) {
+    if (i % 2 === 1) sum += i;
+  }
+  return sum;
+};
--- 02-loops/02-crazy-caps/crazy-caps.js
+++ 02-loops/02-crazy-caps/crazy-caps.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+
+const crazyCaps = (word) => {
+  let changedWord = "";
+  for (let i = 0; i < word.length; i++) {
+    if (i % 2 === 1) {
+      changedWord += word[i].toUpperCase();
+    } else {
+      changedWord += word[i].toLowerCase();
+    }
+  }
+  return changedWord;
+};
