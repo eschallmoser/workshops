bf333845e31fd9ece7b9061deb843b37cfc0993c
Erin Schallmoser <dialogofadream@gmail.com>
2021-11-01 20:53:24 -0700

    practice with arrays

--- 06-arrays-ii/07-my-slice/my-slice.js
+++ 06-arrays-ii/07-my-slice/my-slice.js
@@ -1,1 +1,22 @@
 // YOUR CODE BELOW
+//
+const mySlice = (
+  originalArray,
+  startIdx = 0,
+  endIdx = originalArray.length
+) => {
+  let slicedArr = [];
+
+  if (startIdx < 0) {
+    startIdx = originalArray.length + startIdx;
+  }
+
+  if (endIdx < 0) {
+    endIdx = originalArray.length + endIdx;
+  }
+
+  for (let i = startIdx; i < endIdx; i++) {
+    slicedArr.push(originalArray[i]);
+  }
+  return slicedArr;
+};
